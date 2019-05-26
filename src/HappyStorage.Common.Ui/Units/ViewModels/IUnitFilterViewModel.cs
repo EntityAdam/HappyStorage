ae465c2e693df1ca45cdff1460b7531895b23cdd
Adam Vincent <entityadam@outlook.com>
2019-05-26 10:09:34 -0400

    Cleanup

--- src/HappyStorage.Common.Ui/Units/ViewModels/IUnitFilterViewModel.cs
+++ src/HappyStorage.Common.Ui/Units/ViewModels/IUnitFilterViewModel.cs
@@ -7,18 +7,20 @@ namespace HappyStorage.Common.Ui.Units.ViewModels
 {
     public interface IUnitFilterViewModel
     {
-        bool IsClimateControlled { get; }
-        bool IsVehicleAccessible { get; }
-        int MinimumCubicFeet { get; }
+        bool IsClimateControlled { get; set; }
+        bool IsVehicleAccessible { get; set; }
+        int MinimumCubicFeet { get; set; }
     }
 
     public class UnitFilterViewModel : IUnitFilterViewModel
     {
         [Display(Name = "Climate Controlled?")]
-        public bool IsClimateControlled => true;
+        public bool IsClimateControlled { get; set; }
+
         [Display(Name = "Vehicle Accessible")]
-        public bool IsVehicleAccessible => false;
+        public bool IsVehicleAccessible { get; set; }
+        
         [Display(Name = "Min. Cubic Feet")]
-        public int MinimumCubicFeet => 10;
+        public int MinimumCubicFeet { get; set; }
     }
 }
