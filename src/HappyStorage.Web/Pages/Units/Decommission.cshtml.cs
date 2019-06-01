0b3450a060f9257086fe5ec58d3eaeca8bd01f5a
Adam Vincent <entityadam@outlook.com>
2019-05-31 23:23:14 -0400

    Unit Delete renamed to Decommission, fixed issue.

--- src/HappyStorage.Web/Pages/Units/Decommission.cshtml.cs
+++ src/HappyStorage.Web/Pages/Units/Decommission.cshtml.cs
@@ -13,15 +13,12 @@ namespace HappyStorage.Web.Pages.Unit
 
         public IUnitDecommissionViewModel ViewModel { get; private set; }
 
-        public string UnitNumber { get; set; }
-
-        // => /unit/delete/unit-01
         public void OnGet([FromRoute] string unitNumber)
         {
-            UnitNumber = unitNumber;
+            ViewModel.UnitNumber = unitNumber;
         }
 
-        public IActionResult OnPost([FromForm] string unitNumber)
+        public IActionResult OnPost([FromForm(Name = "ViewModel.UnitNumber")] string unitNumber)
         {
             if (!ModelState.IsValid)
             {
