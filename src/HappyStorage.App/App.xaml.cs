55f847ce77b521187266fea01b57225271e0a0fd
Adam Vincent <entityadam@outlook.com>
2019-06-05 18:08:09 -0400

    Added WPF Project

---
+++ src/HappyStorage.App/App.xaml.cs
@@ -0,0 +1,47 @@
+﻿using HappyStorage.Common.Ui.Extensions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace HappyStorage.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public IServiceProvider ServiceProvider { get; private set; }
+
+        public IConfiguration Configuration { get; private set; }
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var builder = new ConfigurationBuilder();
+             //.SetBasePath(Directory.GetCurrentDirectory())
+             //.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+
+            var serviceCollection = new ServiceCollection();
+            ConfigureServices(serviceCollection);
+
+            ServiceProvider = serviceCollection.BuildServiceProvider();
+
+            var mainWindow = ServiceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHappyStorageCommonUi();
+            services.AddTransient(typeof(MainWindow));
+        }
+    }
+}
