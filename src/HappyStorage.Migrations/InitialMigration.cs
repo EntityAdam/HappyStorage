1bcd1fb40c78fb7c840aab0314eb2376d2811968
Adam Vincent <entityadam@outlook.com>
2019-06-07 11:09:24 -0400

    Adding Docker Support, playing with migrations

---
+++ src/HappyStorage.Migrations/InitialMigration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HappyStorage.Migrations
+{
+    using FluentMigrator;
+
+    namespace TrinugApi.Migrations
+    {
+        /// <summary>
+        /// This migration is just to prove that the migrations *CAN* work.
+        /// </summary>
+        [Migration(1)]
+        public class MigrationInitial : Migration
+        {
+            public override void Up()
+            {
+                Create.Table("trinug_temp_table")
+                    .WithColumn("distribution_id").AsCustom("serial").PrimaryKey()
+                    .WithColumn("text").AsString().NotNullable();
+                Execute.Sql("SELECT create_distributed_table('trinug_temp_table', 'distribution_id')");
+                Delete.Table("trinug_temp_table");
+            }
+            public override void Down()
+            {
+                throw new NotSupportedException();
+            }
+        }
+    }
+}
