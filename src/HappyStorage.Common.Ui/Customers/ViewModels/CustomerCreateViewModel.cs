596b6a30354f5b450bb0b6fb7d5a9da959806c6b
Adam Vincent <entityadam@outlook.com>
2019-07-26 00:38:29 -0400

    Fixed address bug in CustomerCreateViewModel where  CITY, STATE, ZIP was duplicated.

--- src/HappyStorage.Common.Ui/Customers/ViewModels/CustomerCreateViewModel.cs
+++ src/HappyStorage.Common.Ui/Customers/ViewModels/CustomerCreateViewModel.cs
@@ -27,7 +27,7 @@ namespace HappyStorage.Common.Ui.Customers.ViewModels
             {
                 CustomerNumber = Guid.NewGuid().ToString().Split('-')[0],
                 FullName = $"{newCustomer.FirstName} {newCustomer.LastName}",
-                Address = $"{newCustomer.Address}, {newCustomer.City}, {newCustomer.State} {newCustomer.PostalCode}"
+                Address = $"{newCustomer.Address}"
             };
 
             //create
