bdc88b4d85a8a555a9ca93e016ce863b21fe2f6f
Adam Vincent <entityadam@outlook.com>
2021-12-22 09:27:30 -0500

    added dbup project

---
+++ src/HappyStorage.SqlDatabase/Program.cs
@@ -0,0 +1,31 @@
+﻿using DbUp;
+using System.Reflection;
+
+var connectionString =
+    args.FirstOrDefault()
+    ?? @"Data Source=(localdb)\MSSQLLocalDB; Initial Catalog=HappyStorage;";
+
+        
+
+EnsureDatabase.For.SqlDatabase(connectionString);
+
+var upgrader =
+    DeployChanges.To
+        .SqlDatabase(connectionString)
+        .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+        .LogToConsole()
+        .Build();
+
+var result = upgrader.PerformUpgrade();
+
+if (!result.Successful)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(result.Error);
+    Console.ResetColor();
+    return -1;
+}
+Console.ForegroundColor = ConsoleColor.Green;
+Console.WriteLine("Success!");
+Console.ResetColor();
+return 0;
