73c87b64a18296aff4836bf8ddf525fd5e4993f5
Adam Vincent <entityadam@outlook.com>
2021-12-22 15:31:05 -0500

    Added Bogus project

--- src/HappyStorage.Core/Models/NewCustomer.cs
+++ src/HappyStorage.Core/Models/NewCustomer.cs
@@ -1,11 +1,4 @@
 ﻿namespace HappyStorage.Core.Models
 {
     public record NewCustomer(string CustomerNumber, string FullName, string Address);
-    
-    //public class NewCustomer
-    //{
-    //    public string CustomerNumber { get; set; }
-    //    public string FullName { get; set; }
-    //    public string Address { get; set; }
-    //}
 }
